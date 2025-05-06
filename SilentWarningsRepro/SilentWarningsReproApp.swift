75a4303c3b0bffe78b510991239fb398f8738836
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2025-05-06 12:31:16 -0500

    Initial Commit

---
+++ SilentWarningsRepro/SilentWarningsReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentWarningsReproApp.swift
+//  SilentWarningsRepro
+//
+//  Created by Ryan Zulkoski on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SilentWarningsReproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
